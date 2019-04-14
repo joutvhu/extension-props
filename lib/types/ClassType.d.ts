e778380d6807905d3c23e43afe158335be997888
Giáo Hồ <loveme05ye@gmail.com>
2019-04-15 00:29:09 +0700

    Check superclass

--- lib/types/ClassType.d.ts
+++ lib/types/ClassType.d.ts
@@ -7,6 +7,7 @@ interface FunctionType {
     defineClass(name: string, superClass?: any): any;
     preventOverrideClass(classDefinition: any, except?: any[]): boolean;
     preventOverrideFunction(classDefinition: any, functions: string[]): boolean;
+    subclassOf(superClass: any): boolean;
 }
 declare function valueOf(v: any): FunctionType | undefined;
 export declare const type: {
